c5c912732accfc2cd1ca58f3e2a5ba73a3b59c2b
juhongahn <juhong.ahn.dev@gmail.com>
2024-06-28 01:10:51 +0900

    2주차 과제 advanced

--- packages/assignment-2/src/advanced/advanced.jsx
+++ packages/assignment-2/src/advanced/advanced.jsx
@@ -1,62 +1,115 @@
 import { createContext, useContext, useState } from "react";
+export const memo1 = (() => {
+  let store = null;
+  return (fn) => {
+    if (store === null) {
+      store = fn();
+    }
+    return store;
+  };
+})();
 
-export const memo1 = (fn) => fn();
-
-export const memo2 = (fn) => fn();
-
+// b 값이 바뀌더라도 key 값이 [a]로 동일함으로 store에 저장된 값을 반환해야함.
+export const memo2 = (() => {
+  let store = {};
+  return (fn, key) => {
+    if (store[key] === undefined) {
+      store[key] = fn();
+    }
+    return store[key];
+  };
+})();
 
 export const useCustomState = (initValue) => {
-  return useState(initValue);
-}
+  // useState를 호출한다고 해서 리렌더링이 발생하는건 아니다.
+  const [value, setValue] = useState(initValue);
 
-const textContextDefaultValue = {
-  user: null,
-  todoItems: [],
-  count: 0,
+  const proxySetValue = (arg) => {
+    const deepEqualArg = JSON.stringify(arg);
+    const deepEqualExistingValue = JSON.stringify(value);
+
+    if (deepEqualArg === deepEqualExistingValue) return;
+    setValue(arg);
+  };
+  return [value, proxySetValue];
 };
 
-export const TestContext = createContext({
-  value: textContextDefaultValue,
-  setValue: () => null,
-});
+/**
+ * Try 1
+ * 
+ * state들이 한 컴포넌트에 있어서, setxxx로 TestContextProvider 리렌더링시 다른 컴포넌트들이 리렌더링 된다.
+ * 영향을 받지 않도록 분리 시켜주어야함.
+ * 
+  export const TestContextProvider = ({ children }) => {
+    const [counter, setCounter] = useState(0);
+    const [user, setUser] = useState(null);
+    const [todo, setTodo] = useState([]);
+    return (
+      <TestUserContext.Provider value={{ user, setUser }}>
+        <TestTodoContext.Provider value={{ todo, setTodo }}>
+          <TestCounterContext.Provider value={{ counter, setCounter }}>
+            {children}
+          </TestCounterContext.Provider>
+        </TestTodoContext.Provider>
+      </TestUserContext.Provider>
+    );
+  };
+ */
+
+// Try 2
+
+const TestUserContext = createContext();
+const TestTodoContext = createContext();
+const TestCounterContext = createContext();
 
 export const TestContextProvider = ({ children }) => {
-  const [value, setValue] = useState(textContextDefaultValue);
-
   return (
-    <TestContext.Provider value={{ value, setValue }}>
-      {children}
-    </TestContext.Provider>
-  )
-}
+    <TestUserContextProvider>
+      <TestTodoContextProvider>
+        <TestCounterContextProvider>{children}</TestCounterContextProvider>
+      </TestTodoContextProvider>
+    </TestUserContextProvider>
+  );
+};
 
-const useTestContext = () => {
-  return useContext(TestContext);
-}
+const TestUserContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  return (
+    <TestUserContext.Provider value={{ user, setUser }}>
+      {children}
+    </TestUserContext.Provider>
+  );
+};
+
+const TestCounterContextProvider = ({ children }) => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <TestCounterContext.Provider value={{ counter, setCounter }}>
+      {children}
+    </TestCounterContext.Provider>
+  );
+};
+
+const TestTodoContextProvider = ({ children }) => {
+  const [todo, setTodo] = useState([]);
+  return (
+    <TestTodoContext.Provider value={{ todo, setTodo }}>
+      {children}
+    </TestTodoContext.Provider>
+  );
+};
 
 export const useUser = () => {
-  const { value, setValue } = useTestContext();
-
-  return [
-    value.user,
-    (user) => setValue({ ...value, user })
-  ];
-}
+  const { user, setUser } = useContext(TestUserContext);
+  return [user, setUser];
+};
 
 export const useCounter = () => {
-  const { value, setValue } = useTestContext();
-
-  return [
-    value.count,
-    (count) => setValue({ ...value, count })
-  ];
-}
+  const { counter, setCounter } = useContext(TestCounterContext);
+  return [counter, setCounter];
+};
 
 export const useTodoItems = () => {
-  const { value, setValue } = useTestContext();
-
-  return [
-    value.todoItems,
-    (todoItems) => setValue({ ...value, todoItems })
-  ];
-}
+  const { todo, setTodo } = useContext(TestTodoContext);
+  return [todo, setTodo];
+};
