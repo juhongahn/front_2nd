411f96a6cc07f8a305c019f1bea5cea50f0d6785
juhongahn <juhong.ahn.dev@gmail.com>
2024-07-05 00:19:04 +0900

    basic 과제

--- packages/assignment-3/src/basic/MyReact.js
+++ packages/assignment-3/src/basic/MyReact.js
@@ -1,11 +1,47 @@
 import { createHooks } from "./hooks";
-import { render as updateElement } from "./render";
+import { render as updateElement, createElement } from "./render";
 
 function MyReact() {
-  const _render = () => {};
-  function render($root, rootComponent) {}
+  let renderRoot;
+  let renderNode;
+  let oldNode;
 
-  const { useState, useMemo, resetContext: resetHookContext } = createHooks(_render);
+  function resetPrevTestVar() {
+    renderRoot = undefined;
+    renderNode = undefined;
+    oldNode = undefined;
+  }
+
+  /**
+   * setState에서 값이 변경될 때, 호출 될 콜백
+   * 리렌더링이 일어날 때 마다 이전 노드는 지워주고 새 노드를 깔아줘야함.
+   */
+  const _render = () => {
+    resetHookContext();
+    const currNode = renderNode();
+
+    !oldNode
+      ? updateElement(renderRoot, currNode)
+      : updateElement(renderRoot, currNode, oldNode);
+
+    oldNode = currNode;
+  };
+
+  function render($root, rootComponent) {
+    // 이전 테스트 변수 초기화
+    resetPrevTestVar();
+
+    renderRoot = $root;
+    renderNode = rootComponent;
+
+    _render();
+  }
+
+  const {
+    useState,
+    useMemo,
+    resetContext: resetHookContext,
+  } = createHooks(_render);
 
   return { render, useState, useMemo };
 }
--- packages/assignment-3/src/basic/render.js
+++ packages/assignment-3/src/basic/render.js
@@ -1,44 +1,101 @@
 export function jsx(type, props, ...children) {
-  return {}
+  return { type, props, children: children.flat() };
 }
 
 export function createElement(node) {
-  // jsx를 dom으로 변환
+  const { type, props, children = [] } = node;
+
+  const element = document.createElement(type);
+
+  if (props) {
+    Object.entries(props).forEach(([key, value]) => {
+      element.setAttribute(key, value);
+    });
+  }
+
+  children.forEach((node) => {
+    if (typeof node === "string") {
+      element.textContent = node;
+    } else {
+      element.appendChild(createElement(node));
+    }
+  });
+  return element;
 }
 
 function updateAttributes(target, newProps, oldProps) {
-  // newProps들을 반복하여 각 속성과 값을 확인
-  //   만약 oldProps에 같은 속성이 있고 값이 동일하다면
-  //     다음 속성으로 넘어감 (변경 불필요)
-  //   만약 위 조건에 해당하지 않는다면 (속성값이 다르거나 구속성에 없음)
-  //     target에 해당 속성을 새 값으로 설정
+  newProps = newProps || {};
+  oldProps = oldProps || {};
 
-  // oldProps을 반복하여 각 속성 확인
-  //   만약 newProps들에 해당 속성이 존재한다면
-  //     다음 속성으로 넘어감 (속성 유지 필요)
-  //   만약 newProps들에 해당 속성이 존재하지 않는다면
-  //     target에서 해당 속성을 제거
+  Object.entries(newProps).forEach(([name, value]) => {
+    if (oldProps[name] !== value) {
+      target.setAttribute(prop, newProps[prop]);
+    }
+  });
+
+  Object.keys(oldProps).forEach((name) => {
+    if (newProps[name] === undefined) {
+      target.removeAttribute(name);
+    }
+  });
 }
 
+/**
+ * 단순히 jsx를 렌더링하는 함수가 아니라 oldNode를 newNode로 변환
+ * */
 export function render(parent, newNode, oldNode, index = 0) {
   // 1. 만약 newNode가 없고 oldNode만 있다면
   //   parent에서 oldNode를 제거
   //   종료
+  if (!newNode && oldNode) {
+    parent.removeChild(createElement(oldNode));
+    return;
+  }
 
   // 2. 만약 newNode가 있고 oldNode가 없다면
   //   newNode를 생성하여 parent에 추가
   //   종료
+  if (newNode && !oldNode) {
+    parent.appendChild(createElement(newNode));
+    return;
+  }
 
   // 3. 만약 newNode와 oldNode 둘 다 문자열이고 서로 다르다면
   //   oldNode를 newNode로 교체
   //   종료
+  if (
+    typeof newNode === "string" &&
+    typeof oldNode === "string" &&
+    newNode !== oldNode
+  ) {
+    parent.replaceChild(createElement(newNode), createElement(oldNode));
+    return;
+  }
 
   // 4. 만약 newNode와 oldNode의 타입이 다르다면
   //   oldNode를 newNode로 교체
   //   종료
+  if (newNode.nodeType !== oldNode.nodeType) {
+    parent.replaceChild(createElement(newNode), createElement(oldNode));
+    return;
+  }
+
+  // 부모 DOM은 변경 대상에 제외 해야하므로 -> parent.childNodes[index]
 
   // 5. newNode와 oldNode에 대해 updateAttributes 실행
+  updateAttributes(parent.childNodes[index], newNode.props, oldNode.props);
 
   // 6. newNode와 oldNode 자식노드들 중 더 긴 길이를 가진 것을 기준으로 반복
   //   각 자식노드에 대해 재귀적으로 render 함수 호출
+  const newChildren = newNode.children || [];
+  const oldChildren = oldNode.children || [];
+  const maxLength = Math.max(newChildren.length, oldChildren.length);
+  for (let i = 0; i < maxLength; i++) {
+    render(
+      parent.childNodes[index],
+      newNode.children[i],
+      oldNode.children[i],
+      i
+    );
+  }
 }
