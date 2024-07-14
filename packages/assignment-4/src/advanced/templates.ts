d2b5248d85f156cf85e979029bcaa2f6fa6c9cab
juhongahn <juhong.ahn.dev@gmail.com>
2024-07-14 12:41:29 +0900

    resolve confict

--- packages/assignment-4/src/advanced/templates.ts
+++ packages/assignment-4/src/advanced/templates.ts
@@ -15,7 +15,7 @@ export const getMainLayoutLiteral = ({ items }: { items: Product[] }) => {
       <h1 class="text-2xl font-bold mb-4">장바구니</h1>
         <div id="cart-items"></div>
         <div id="cart-total" class="text-xl font-bold my-4"></div>
-        <select id="product-select" class="border rounded p-2 mr-2">
+        <select NEWid="product-select" class="border rounded p-2 mr-2">
         ${optionLiteral}
       </select>
         <button id="add-to-cart" class="bg-blue-500 text-white px-4 py-2 rounded">추가</button>
