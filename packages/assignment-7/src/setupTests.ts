5ae06d3b330be9985a598705fadbe486dfbd8e44
juhongahn <juhong.ahn.dev@gmail.com>
2024-07-30 21:51:08 +0900

    test setup

--- packages/assignment-7/src/setupTests.ts
+++ packages/assignment-7/src/setupTests.ts
@@ -1,1 +1,16 @@
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import { setupServer } from "msw/node";
+import { mockCalendarApiHandler } from "./mockCalendarApiHandler";
+
+const server = setupServer(...mockCalendarApiHandler);
+
+beforeAll(() => server.listen());
+
+afterEach(() => {
+  server.resetHandlers();
+  vi.clearAllMocks();
+});
+afterAll(() => {
+  server.close();
+  vi.clearAllMocks();
+});
