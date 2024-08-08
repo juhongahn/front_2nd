9bc257784c05906be56adfd12a748929745a4301
juhongahn <juhong.ahn.dev@gmail.com>
2024-08-09 04:18:14 +0900

    fix: add createMockHandler

--- packages/assignment-8/src/main.tsx
+++ packages/assignment-8/src/main.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
+import { initalEvents } from "./mockCalendarApiHandler.ts";
 
 async function prepare() {
   const { setupWorker } = await import("msw/browser");
-  const { mockCalendarApiHandler } = await import(
-    "./mockCalendarApiHandler.ts"
-  );
-  const worker = setupWorker(...mockCalendarApiHandler);
+  const { createMockHandler } = await import("./mockCalendarApiHandler.ts");
+  const worker = setupWorker();
+  worker.use(...createMockHandler(initalEvents));
   return worker.start();
 }
 prepare().then(() => {
