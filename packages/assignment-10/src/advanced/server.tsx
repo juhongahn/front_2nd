b4492f760883630ebbb1bb7b40fc46c336860da8
juhongahn <juhong.ahn.dev@gmail.com>
2024-08-23 01:25:29 +0900

    assignment-10 advanced

--- packages/assignment-10/src/advanced/server.tsx
+++ packages/assignment-10/src/advanced/server.tsx
@@ -1,17 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import React from 'react';
-import express from 'express';
-import ReactDOMServer from 'react-dom/server';
-import { App } from './App.tsx';
+import React from "react";
+import express from "express";
+import ReactDOMServer from "react-dom/server";
+import { App } from "./App.tsx";
 
 const app = express();
 const port = 3333;
 
-app.get('*', (req, res) => {
-  const app = ReactDOMServer.renderToString(<App url={req.url}/>);
+const cacheMap = new Map<string, { content: string; timestamp: number }>();
+const CACHE_AGE = 5 * 60 * 1000;
 
-  res.send(`
+const renderApp = (url: string) => {
+  const app = ReactDOMServer.renderToString(<App url={url} />);
+
+  return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -23,7 +26,29 @@ app.get('*', (req, res) => {
       <div id="root">${app}</div>
     </body>
     </html>
-  `);
+  `;
+};
+
+app.get("*", (req, res) => {
+  const cacheKey = req.url;
+  const now = Date.now();
+  const cachedItem = cacheMap.get(cacheKey);
+
+  if (cachedItem && now - cachedItem.timestamp < CACHE_AGE) {
+    return res.send(cachedItem.content);
+  }
+
+  const content = renderApp(req.url);
+  cacheMap.set(cacheKey, { content, timestamp: now });
+  res.send(content);
+
+  if (cacheMap.size > 100) {
+    const oldestKey = [...cacheMap.keys()].sort(
+      (a, b) =>
+        (cacheMap.get(a)?.timestamp || 0) - (cacheMap.get(b)?.timestamp || 0),
+    )[0];
+    cacheMap.delete(oldestKey);
+  }
 });
 
 app.listen(port, () => {
